1cf17bc8acfbab0a00bfe84eb6d7adec46d21175
Dan Wolff <fondiller@gmail.com>
2011-03-10 19:52:18 +0100

    big_base > standard_big

--- tests/standard_big.js
+++ tests/standard_big.js
@@ -1,9 +1,9 @@
-module("big_base");
+module("standard_big");
 
 test("Basic requirements", function () {
 	expect(2);
 	ok(Base, "Base");
-	ok(Base.extensions.big_base, "Base.extensions.big_base");
+	ok(Base.extensions.standard_big, "Base.extensions.standard_big");
 });
 
 test("Valid base", function () {
@@ -12,16 +12,28 @@ test("Valid base", function () {
 	strictEqual(Base.valid(" 46"), false, "preceding space");
 	strictEqual(Base.valid(37), true, "37");
 	strictEqual(Base.valid(4.256), false, "4.256");
-	strictEqual(Base.valid(50), true, "50");
+	strictEqual(Base.valid("50"), true, '"50"');
 	strictEqual(Base.valid(85), true, "85");
 	strictEqual(Base.valid(1337), true, "1337");
 });
 
-test("Convert from bases", function () {
+test("Valid base and number", function () {
+	expect(6);
+	
+	strictEqual(Base.valid(" 46", "6"), false, "preceding space");
+	strictEqual(Base.valid(37, "36:4:1:0.1:6:1:000:006"), true, "36:4:1:0.1:6:1:000:006 in base 37");
+	strictEqual(Base.valid(4.256, "1"), false, "1 in base 4.256");
+	strictEqual(Base.valid("50", "50"), false, "50 in base 50");
+	strictEqual(Base.valid(85, ".84:84"), true, ".84:84 in base 85");
+	strictEqual(Base.valid(1337, "1234.1330:512"), true, "1234.1330:512 in base 1337");
+});
+
+test("Convert to and from bases", function () {
 	var i, kv, from, to, undefined,
 		conversions = [
 			{ from: 100, to: 10,
 				"-000005.0000": "-5",
+				"-0:000:00:0005": "-5",
 				"-0": "0",
 				"-.5": "-0.05",
 				".62:50": "0.625",
@@ -34,7 +46,13 @@ test("Convert from bases", function () {
 				"A": undefined,
 				"BCD": undefined,
 				"0..5": undefined,
-				"42.375": undefined
+				"42.375": undefined,
+				"00055555": undefined
+			},
+			{ from: "100", to: "10",
+				"00555": undefined,
+				"15": "15",
+				"6": "6"
 			},
 			{ from: 37, to: 10,
 				"36:36": "1368",
@@ -47,10 +65,6 @@ test("Convert from bases", function () {
 			}
 		];
 	
-//	strictEqual(Base.valid(100, "12:3"), true, "Base.valid(100, '12:3')");
-	strEqual(Base.from(100, "-.5"), "-0.05", "testing");
-//	strEqual(Base.to(100, 1203), "12:3", "testing 1203 > 12:3");
-	return;
 	expect(conversions.length);
 	
 	function keys_and_values(obj) {
