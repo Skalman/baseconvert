8cc3d601c5f05f8d2e1724b780aa25a0a926fdbb
Dan Wolff <fondiller@gmail.com>
2011-03-17 18:34:57 +0100

    moving from_internal commons to a generic function

--- standard.js
+++ standard.js
@@ -1,8 +1,8 @@
-// TODO move from_internal logic to separate file
 (function (Base) {
 	"use strict";
 	var dictionary = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 		Number = Base.Number,
+		ZERO = Number.ZERO,
 		valid_number = [],
 		valid_number_big = /^\-?(\d+(\:\d+)*)?\.?(\d+(\:\d+)*)?$/,
 		base_names = {2: "binary", 8: "octal", 10: "decimal", 16: "hexadecimal"},
@@ -19,6 +19,75 @@
 			return base_name_generic.replace("#base#", base);
 		}
 	}
+	function standard_from_internal_generic(to_base, number) {
+		if (number.equals(ZERO)) {
+			return {
+				digits: [0],
+				neg: false,
+				pt: false
+			};
+		}
+		number = number.clone(); // we must work on a copy
+		var tmp, i,
+			digits = [],
+			is_negative = number.cmp(0) < 0,
+			integ = Number.floor(number.abs()), // the integer part; make the number non-negative
+			fract = number.mod(1), // the fractional part; we don't need the 'number' variable any more
+			significant_digits = 0,
+
+			// number of significant digits we can safely handle (53 bits)
+			// TODO - don't hard code this - perhaps number.PRECISION? How to handle those that do integer part and fractional part separately?
+			max_significant_digits = 53 * Math.log(2) / Math.log(to_base),
+			integer_length;
+
+		// find the integer part of the result
+		for (; integ.cmp(ZERO) > 0; significant_digits++) {
+			tmp = Number.mod(integ, to_base).get_number();
+			digits.unshift(tmp);
+			integ.sub(tmp).div(to_base);
+		}
+		integer_length = digits.length;
+		// find the fractional part of the result
+		for (; significant_digits < max_significant_digits && !fract.equals(ZERO); significant_digits++) {
+			fract.mul(to_base);
+			tmp = Number.floor(fract);
+			fract.sub(tmp);
+			digits.push(tmp.get_number());
+		}
+
+		if (significant_digits >= max_significant_digits) {
+			// round (away from zero)
+			if (digits.pop() >= to_base / 2) {
+				tmp = digits.length;
+
+				// add 1 to the last element, but if it's to_base, we'll have to remove it, and check the next
+				while (++digits[--tmp] === to_base) {
+					if (tmp > integer_length) {
+						// we're behind the decimal point, remove the last digit
+						digits.pop();
+					} else {
+						// we're in the integer part - can't remove this digit: instead set it to zero
+						digits[tmp] = 0;
+					}
+				}
+			} else {
+				tmp = digits.length;
+				while (digits[--tmp] === 0 && tmp > integer_length) {
+					digits.pop();
+				}
+			}
+		}
+		if (integer_length === 0) {
+			// we don't return results with beginning with '.'
+			digits.unshift(0);
+			integer_length++;
+		}
+		return {
+			digits: digits,
+			neg: is_negative,
+			pt: integer_length === digits.length ? false : integer_length
+		};
+	}
 	Base.extend({
 		name: "standard",
 		/* bool valid_base(string) */
@@ -29,14 +98,14 @@
 		valid_from: function standard_valid_from(base, number) {
 			return (
 				// eliminate the strings that the RegExp can't handle
-				number !== "" && number !== "." && number !== "-." && number !== "-"
+				number !== "" && number !== "." && number !== "-." && number !== "-" &&
 
 				// valid base
-				&& 2 <= base && base <= 36
-				&& parseInt(base, 10)+"" === base
+				2 <= base && base <= 36 &&
+				parseInt(base, 10)+"" === base &&
 
 				// get the validator RegExp
-				&& (valid_number[base] ? valid_number[base] : get_validator(base))
+				(valid_number[base] ? valid_number[base] : get_validator(base))
 					// and test the number on that RegExp
 					.test(number)
 			);
@@ -46,8 +115,8 @@
 			return (
 				// any real number is fine
 				// valid base
-				2 <= base && base <= 36
-				&& parseInt(base, 10)+"" === base
+				2 <= base && base <= 36 &&
+				parseInt(base, 10)+"" === base
 			);
 		},
 		fractional: true,
@@ -93,90 +162,22 @@
 		},
 
 
-		// parameters number and base
 		from_internal: function standard_from_internal(to_base, number) {
-			number = number.clone(); // we must work on a copy
-			if (number.equals(Number.ZERO)) {
-				return "0";
+			var i, length,
+				result = standard_from_internal_generic(+to_base, number),
+				digits = result.digits,
+				pt = result.pt;
+			for (i = 0, length = digits.length; i < length; i++) {
+				digits[i] = dictionary[ digits[i] ];
 			}
-			var tmp, i,
-				is_negative = number.cmp(0) < 0, // negative ?
-				result = [],
-				result_fract,
-				fract = Number.mod(number.abs(), 1), // the fractional part; make the number non-negative
-				significant_digits = 0,
-				max_significant_digits,
-				integer_digits;
-
-			number.floor();
-
-			// number of significant digits we can safely handle (53 bits)
-			// TODO - don't hard code this - perhaps number.PRECISION? How to handle those that do integer part and fractional part separately?
-			max_significant_digits = 53 * Math.log(2) / Math.log(to_base);
-
-
-			// find the integer part of the result
-			if (number.equals(Number.ZERO)) {
-				result.push(0);
-			} else {
-				for (; number.cmp(0) > 0; significant_digits++) {
-					tmp = Number.mod(number, to_base).get_number();
-					result.unshift(tmp);
-					// number = (number - tmp) / to;
-					number.sub(tmp).div(to_base);
-				}
-			}
-			integer_digits = result.length;
-			// find the fractional part of the result
-			if (!fract.equals(Number.ZERO) && significant_digits < max_significant_digits) {
-				for (; significant_digits < max_significant_digits; significant_digits++) {
-					// fract *= to;
-					fract.mul(to_base);
-					tmp = Number.floor(fract);
-					// fract -= tmp;
-					fract.sub(tmp);
-					result.push(tmp.get_number());
-				}
-
-				// round (away from zero)
-				if (result.pop() >= to_base / 2) {
-					tmp = result.length;
-
-					// add 1 to the last element, but if it's to_base, we'll have to remove it, and check the next
-					while (++result[--tmp] == to_base) {
-						if (tmp > integer_digits) {
-							// we're behind the decimal point, remove the last digit
-							result.pop();
-						} else {
-							// we're in the integer part - can't remove this digit: instead set it to zero
-							result[tmp] = 0;
-						}
-					}
-				} else {
-					tmp = result.length;
-					while (result[--tmp] === 0 && tmp > integer_digits) {
-						result.pop();
-					}
-				}
-				for (i = result.length - 1; i >= 0; i--) {
-					result[i] = dictionary.charAt(result[i]);
-				}
-				if (integer_digits >= result.length) {
-					result = result.join("");
-				} else {
-					result = result.slice(0, integer_digits).join("") + "." + result.slice(integer_digits).join("");
-				}
-			} else {
-				for (i = result.length - 1; i >= 0; i--) {
-					result[i] = dictionary.charAt(result[i]);
-				}
-				result = result.join("");
-			}
-
-			if (is_negative) {
-				result = "-" + result;
-			}
-			return result;
+			return (
+				(result.neg ? "-" : "") +
+				(pt ?
+					// a decimal point
+					digits.slice(0, pt).join("") + "." + digits.slice(pt).join("") :
+					// no decimal point - just join
+					digits.join(""))
+			);
 		},
 		get_name: standard_get_name,
 		options: {
@@ -196,13 +197,13 @@
 		valid_from: function standard_valid_from(base, number) {
 			if (
 				// eliminate the strings that the RegExp can't handle
-				number !== "" && number !== "." && number !== "-." && number !== "-"
+				number !== "" && number !== "." && number !== "-." && number !== "-" &&
 
 				// valid base
-				&& 36 < base
-				&& parseInt(base, 10)+"" === base
+				36 < base &&
+				parseInt(base, 10)+"" === base &&
 
-				&& valid_number_big.test(number)
+				valid_number_big.test(number)
 			) {
 				number = number.split(/[\-\.\:]/);
 				base = parseInt(base, 10);
@@ -220,8 +221,8 @@
 			return (
 				// any real number is fine
 				// valid base
-				36 < base
-				&& parseInt(base, 10)+"" === base
+				36 < base &&
+				parseInt(base, 10)+"" === base
 			);
 		},
 		fractional: true,
@@ -264,88 +265,22 @@
 		},
 
 
-		// parameters number and base
-		from_internal: function standard_from_internal(to_base, number) {
-			number = number.clone(); // we must work on a copy
-			if (number.equals(Number.ZERO)) {
-				return "0";
-			}
-			var tmp,
-				is_negative = number.cmp(0) < 0, // negative ?
-				result = [],
-				result_fract,
-				fract = Number.mod(number.abs(), 1), // the fractional part; make the number non-negative
-				significant_digits = 0,
-				max_significant_digits,
-				integer_digits;
-
-			number.floor();
-
-			// number of significant digits we can safely handle (53 bits)
-			// TODO - don't hard code this - perhaps number.PRECISION? How to handle those that do integer part and fractional part separately?
-			max_significant_digits = 53 * Math.log(2) / Math.log(to_base);
-
-
-			// find the integer part of the result
-			if (number.equals(Number.ZERO)) {
-				result.push(0);
-			} else {
-				for (; number.cmp(0) > 0; significant_digits++) {
-					tmp = Number.mod(number, to_base).get_number();
-					result.unshift(tmp);
-					// number = (number - tmp) / to;
-					number.sub(tmp).div(to_base);
-				}
-			}
-			integer_digits = result.length;
-			// find the fractional part of the result
-			if (!fract.equals(Number.ZERO) && significant_digits < max_significant_digits) {
-				for (; significant_digits < max_significant_digits; significant_digits++) {
-					// fract *= to;
-					fract.mul(to_base);
-					tmp = Number.floor(fract);
-					// fract -= tmp;
-					fract.sub(tmp);
-					result.push(tmp.get_number());
-				}
-
-				// round (away from zero)
-				if (result.pop() >= to_base / 2) {
-					tmp = result.length;
-
-					// add 1 to the last element, but if it's to_base, we'll have to remove it, and check the next
-					while (++result[--tmp] == to_base) {
-						if (tmp > integer_digits) {
-							// we're behind the decimal point, remove the last digit
-							result.pop();
-						} else {
-							// we're in the integer part - can't remove this digit: instead set it to zero
-							result[tmp] = 0;
-						}
-					}
-				} else {
-					tmp = result.length;
-					while (result[--tmp] === 0 && tmp > integer_digits) {
-						result.pop();
-					}
-				}
-				if (integer_digits >= result.length) {
-					result = result.join(":");
-				} else {
-					result = result.slice(0, integer_digits).join(":") + "." + result.slice(integer_digits).join(":");
-				}
-			} else {
-				result = result.join(":");
-			}
-
-			if (is_negative) {
-				result = "-" + result;
-			}
-			return result;
+		from_internal: function standard_big_from_internal(to_base, number) {
+			var i, length,
+				result = standard_from_internal_generic(+to_base, number),
+				digits = result.digits,
+				pt = result.pt;
+			return (
+				(result.neg ? "-" : "") +
+				(pt ?
+					// a decimal point
+					digits.slice(0, pt).join(":") + "." + digits.slice(pt).join(":") :
+					// no decimal point - just join
+					digits.join(":"))
+			);
 		},
 		get_name: standard_get_name,
 		options: {
 		}
 	});
-})(Base);
-
+}(Base));
